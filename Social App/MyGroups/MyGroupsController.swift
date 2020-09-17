ac68e5f2b0c4a4d20b77d2aaf8550d1d89d2f316
LeshaAleshaaa <alexeysmith3@gmail.com>
2020-09-17 21:12:33 +0300

    Создание экранов групп

--- Social App/MyGroups/MyGroupsController.swift
+++ Social App/MyGroups/MyGroupsController.swift
@@ -14,6 +14,8 @@ final class MyGroupsController: UITableViewController {
     
     // MARK: - Private properties
     
+    private lazy var myGroupsList = [String]()
+    private lazy var myGroupsImage = [UIImage]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,71 +26,50 @@ final class MyGroupsController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    // MARK: - Actions
+    
+    @IBAction func addGroup(segue: UIStoryboardSegue) {
+        
+        if segue.identifier == "addGroup" {
+            let newGroupsController = segue.source as! NewGroupsController
+            if let indexPath = newGroupsController.tableView.indexPathForSelectedRow {
+                let groupName = newGroupsController.groupsList[indexPath.row]
+                let groupImage = newGroupsController.groupsImage[indexPath.row]
+                
+                if !myGroupsList.contains(groupName) {
+                myGroupsList.append(groupName)
+                myGroupsImage.append(groupImage!)
+                
+                tableView.reloadData()
+            }
+        }
+        }
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return myGroupsList.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "myGroupsCell", for: indexPath) as? MyGroupsCell else { return UITableViewCell() }
 
-        // Configure the cell...
-
+        cell.myGroupName.text = myGroupsList[indexPath.row]
+        cell.myGroupImage.image = myGroupsImage[indexPath.row]
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            myGroupsList.remove(at: indexPath.row)
+            myGroupsImage.remove(at: indexPath.row)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
